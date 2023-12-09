e7b1c1754ab6287750bf3ffbf9e227eba6484b29
josephmangara <josephmangara2@gmail.com>
2023-12-09 17:07:21 +0300

    complet

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ function happyNewYear() {
   }
   console.log("Happy New Year!");
 }
-
+happyNewYear()
 /* 
   Write a method `fizzbuzz_printer` that prints the numbers from 1 to 100. For
   multiples of three, print "Fizz" instead of the number and for the multiples
